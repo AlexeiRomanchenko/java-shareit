546b40646976b64595a0c5ef372b16b8466bcd2d
AlexeiRomanchenko <119013069+AlexeiRomanchenko@users.noreply.github.com>
2023-08-19 15:48:10 +0300

    fix: исправлены замечания РЕвьюера. Исправлена N+1 проблема

--- src/main/java/ru/practicum/shareit/booking/dto/ShortItemBookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/ShortItemBookingDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.booking.description.BookingStatus;
 
 import java.time.LocalDateTime;
 
