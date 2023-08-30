3c52e2206443b9c9e9dcbfbe421f67caeddfcb35
AlexeiRomanchenko <119013069+AlexeiRomanchenko@users.noreply.github.com>
2023-08-30 18:55:05 +0300

    Добавлен тест approveWhenBookingDecisionTest()

--- src/main/java/ru/practicum/shareit/request/service/FromSizeRequest.java
+++ src/main/java/ru/practicum/shareit/request/service/FromSizeRequest.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Sort;
 
 public class FromSizeRequest extends PageRequest {
     private final int from;
+
     public FromSizeRequest(int from, int size, Sort sort) {
         super(from / size, size, sort);
         this.from = from;
@@ -13,6 +14,7 @@ public class FromSizeRequest extends PageRequest {
     public static FromSizeRequest of(int from, int size) {
         return of(from, size, Sort.unsorted());
     }
+
     public static FromSizeRequest of(int from, int size, Sort sort) {
         return new FromSizeRequest(from, size, sort);
     }
